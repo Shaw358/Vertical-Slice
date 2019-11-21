c803fff102d4a2b74abb50220aeda0420e369ea6
IbrahimM04 <43440821+IbrahimM04@users.noreply.github.com>
2019-11-21 11:23:51 +0100

    UI & Scripts

---
+++ Angry Birds - VS1/Assets/Scripts/MoveScripts/MoveScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public float speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(speed, 0) *Time.deltaTime;
+    }
+}
---
+++ Angry Birds - VS1/Assets/Scripts/UI/PauseScreenScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScreenScript : MonoBehaviour {
+    public GameObject pauseScreen;
+    public GameObject pauseButton;
+    public Transform canvas;
+    public AudioListener audioListener;
+    public List<AudioSource> audioSources;
+    private bool opened = false;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            OpenPauseScreen();
+            opened = true;
+        } else if (opened == true) {
+            if (Input.GetKeyDown(KeyCode.Escape)) {
+                ClosePauseScreen();
+            }
+        }
+    }
+
+    public void OpenPauseScreen() {
+        pauseScreen.SetActive(true);
+        pauseButton.SetActive(false);
+        Time.timeScale = 0f;
+    }
+
+    public void ClosePauseScreen() {
+        pauseScreen.SetActive(false);
+        pauseButton.SetActive(true);
+        Time.timeScale = 1.0f;
+    }
+
+    public void ResetGame() {
+        SceneManager.LoadScene("SampleScene");
+        Time.timeScale = 1.0f;
+    }
+
+    public void MuteSounds() {
+        foreach (AudioSource audioSorce in audioSources) {
+            audioSorce.Stop();
+        }
+    }
+}
+
